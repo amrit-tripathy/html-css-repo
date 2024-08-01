28d61bc77087529e94be00e2fd87b00b71d00c9c
amrit-tripathy <124559864+amrit-tripathy@users.noreply.github.com>
2024-08-01 06:48:03 +0200

    Add files via upload

---
+++ html/31st-july/demoOnswitch.js
@@ -0,0 +1,11 @@
+var n=1;
+switch(n){
+    case 1:console.log("hi");
+    break;
+    case 2:console.log("hii");
+    break;
+    case 3:console.log("hiii");
+    break;
+    case 4:console.log("hiiii");
+    break;
+}
